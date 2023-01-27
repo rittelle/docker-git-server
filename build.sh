dd0a88b7609693de737f15adeba468e339276b55
rittelle <33598633+rittelle@users.noreply.github.com>
2023-01-27 15:50:05 +0100

    Add a check preventing dirty commits being pushed
    Change the build script so that the image is not being pushed as long as
    the repo is dirty.

--- build.sh
+++ build.sh
@@ -6,10 +6,19 @@ NAME="rittelle/git-server"
 VERSION="$(git rev-parse --verify HEAD)"
 ARCHITECTURES="linux/amd64,linux/arm64"
 
-docker buildx build \
-    --platform "$ARCHITECTURES" \
-    -t "$NAME:$VERSION" \
-    -t "$NAME:latest" \
-    --push \
-    .
+if not git diff-files --quiet ; then
+    echo "You seem to have uncommited changes, I wont push the image"
 
+    docker buildx build \
+        --platform "$ARCHITECTURES" \
+        -t "$NAME:$VERSION-dirty" \
+        -t "$NAME:latest" \
+        .
+else
+    docker buildx build \
+        --platform "$ARCHITECTURES" \
+        -t "$NAME:$VERSION" \
+        -t "$NAME:latest" \
+        --push \
+        .
+fi
